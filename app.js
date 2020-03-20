f68877d5db028c3ce1e572c7bc67792468099d00
Amartinlopez <andre.martin.lopez@gmail.com>
2020-03-20 13:02:17 +0100

    principio back

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const apiRouter = require('./routes/api');
 
 require('dotenv').config();
 var app = express();
@@ -26,5 +27,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/api', apiRouter);
 
 module.exports = app;
