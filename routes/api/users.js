9a1459d10a8c7edebe13517a0bbdea74c27f79d0
Amartinlopez <andre.martin.lopez@gmail.com>
2020-03-22 13:07:50 +0100

    Dia 22.03.2020 (15 días más de cuarentena)

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,40 +1,40 @@
 const router = require('express').Router();
 const User = require('../../model/user');
+const bcrypt = require('bcryptjs')
 
 
 //Get http://localhost:3000/api/users
-router.get('/',async(req,res)=>{
+router.get('/', async (req, res) => {
     // console.log(req.payload);
     const rows = await User.getAll()
     res.json(rows);
 });
 //POST http://localhost:3000/api/users
-router.post('/',async (req,res)=>{
-    const result = await User.create(req.body.nombre,req.body.apellidos,req.body.fecha_nacimiento,req.body.email, req.body.contraseña,req.body.repite_contraseña);
-    if(result['affectedRows'] === 1){
-        const user = await User.getById(result['insertId']);
-        res.json(user)
-    }else{
-        res.json({error:"el usuario no esta registrado"})
-    }
+router.post('/', async (req, res) => {
+    const contraseñaEnc = bcrypt.hashSync(req.body.contraseña, 10);
+    req.body.contraseña = contraseñaEnc
+    const result = await User.create(req.body.nombre, req.body.apellidos, req.body.fecha_nacimiento, req.body.email, req.body.contraseña, req.body.repite_contraseña)
+    res.json(result)
+
 });
+
 //PUT http://localhost:3000/api/users/:pUserId
-router.put('/:pUserId',async (req,res)=>{
-    
-     console.log(req.body);
-     console.log(req.params);
-     const result = await User.updateById(req.body, req.params.pUserId);
+router.put('/:pUserId', async (req, res) => {
+
+    console.log(req.body);
+    console.log(req.params);
+    const result = await User.updateById(req.body, req.params.pUserId);
     //  console.log(result);
     res.json(result)
 });
 //DELETE http://localhost:3000/api/users
-router.delete('/',async(req,res)=>{
+router.delete('/', async (req, res) => {
     console.log(req.body);
-    const result = await User.deleteById (req.body.id);
-    if(result ['affectedRows'] === 1){
-        res.json({success:'El usuario ha sido borrado'});
-    }else{
-        res.json({error:'el usuario no ha sido eliminado'})
+    const result = await User.deleteById(req.body.id);
+    if (result['affectedRows'] === 1) {
+        res.json({ success: 'El usuario ha sido borrado' });
+    } else {
+        res.json({ error: 'el usuario no ha sido eliminado' })
     }
 });
 module.exports = router;
